8a0ba0bd0ec083bccbecb1d54b7f3c2d7562cb75
Daniel Borowski <danielborowski@Daniels-MBP-5.fios-router.home>
2020-05-11 15:59:00 -0400

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -179,7 +179,7 @@ export default ({ pageContext: { featuredArticles } }) => {
                         <br />
                         {`["fast", "innovative", "original"]})`}
                     </Heading>
-                    <Sub>What will you create toaaaaaadsakdasnfaajsd22nfsdfay?</Sub>
+                    <Sub>What will you create toaaaaaadsakdasnfaaajsd22nfsdfay?</Sub>
                     <CardGallery>
                         {featuredArticles.map(article => {
                             const {
