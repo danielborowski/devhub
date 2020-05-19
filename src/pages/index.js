ac69f1520152a03830a6be723abc20317e6bbb0c
Daniel Borowski <danielborowski@Daniels-MBP-5.fios-router.home>
2020-05-19 13:24:34 -0400

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -179,7 +179,7 @@ export default ({ pageContext: { featuredArticles } }) => {
                         <br />
                         {`["fast", "innovative", "original"]})`}
                     </Heading>
-                    <Sub>What will you create today?</Sub>
+                    <Sub>What will you create tTOTOOTOTOTOTOTToday?</Sub>
                     <CardGallery>
                         {featuredArticles.map(article => {
                             const {
